9c8a117904df2393e107b8d5b8fb6d94b8e109e8
Daniel Tatiyama <Daniel Tatiyama>
2016-01-25 20:27:05 -0200

    Push inicial com os arquivos do discordjs.

---
+++ app.js
@@ -0,0 +1,18 @@
+var Discord = require("discord.js");
+ 
+var mybot = new Discord.Client();
+ 
+mybot.on("message", function(message){
+	switch(message.content){
+		case "ping":
+			mybot.reply(message, "pong");
+			break;
+		case "$Logout":
+			mybot.logout()
+		default:
+			break;
+	}
+
+});
+
+mybot.login("danielhtatiyama@gmail.com", "afferson");
