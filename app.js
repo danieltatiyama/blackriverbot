878b38e06bb6b2be3cc164c93b6831581299fa8b
Daniel Hideki Tatiyama <dtatiyama@SJRP.CSC.COM>
2016-01-26 09:46:58 -0200

    Ajustado texto de uptime.

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var moment = require('moment');
 
 var mybot = new Discord.Client();
 var uptimesince = moment().format("DD/MM/YYYY - HH:mm:ss");
-var uptimefor = moment().startOf('hour', uptimesince).fromNow();
-
 
 mybot.on("message", function(message){
 	switch(message.content){
@@ -18,7 +16,7 @@ mybot.on("message", function(message){
 			mybot.sendMessage(message.channel, "I've been functioning since " + uptimesince );
 			var uptimefor = moment().startOf('minutes', uptimesince).fromNow();
 			setTimeout(function() {
-			    mybot.sendMessage(message.channel, "Which means I've been running ok for " + uptimefor );
+			    mybot.sendMessage(message.channel, "In other words, I've been on since a " + uptimefor );
 			}, 1000);			
 		default:
 			break;
